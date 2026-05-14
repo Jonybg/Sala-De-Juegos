c1514164e2abeea0a69aefa959b44cfc150999f3
jonybello123 <jonathanbellogabriel@gmail.com>
2026-05-13 23:25:55 -0300

    arreglo bugs del login para la entrega

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -9,10 +9,10 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 export class Register {
  form = new FormGroup({
     email:    new FormControl('', [Validators.required, Validators.email]),
-    nombre:   new FormControl('', [Validators.required]),
-    apellido: new FormControl('', [Validators.required]),
-    edad:     new FormControl<number | null>(null, [Validators.required, Validators.min(13)]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    nombre:   new FormControl('', [Validators.required,Validators.minLength(2),Validators.maxLength(15)]),
+    apellido: new FormControl('', [Validators.required,Validators.minLength(2),Validators.maxLength(15)]),
+    edad:     new FormControl<number | null>(null, [Validators.required, Validators.min(13),Validators.max(120)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6),Validators.maxLength(15)]),
   });
 
   onSubmit() {
